d521f7571c53f09cf8df718eeebfb5412f36993e
sojib01943075658@gmail.com <sojib01943075658@gmail.com>
2023-06-20 01:23:16 +0600

    v.3

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# Spck-editor
+
+C
+(
+
+Description
+The Calculator program is a simple command-line application that allows you to perform basic arithmetic operations. It is designed to help you quickly and easily perform calculations without the need for complex software.
+
+Features
+
+Addition: Perform addition of two numbers.
+Subtraction: Perform subtraction of two numbers.
+Multiplication: Perform multiplication of two numbers.
+Division: Perform division of two numbers.
+Modulo: Calculate the remainder of division between two numbers.
+Installation
+To use the Calculator program, follow the instructions below:
+
+Clone or download the Calculator repository from GitHub: https://github.com/yourusername/calculator.
+Make sure you have Python 3 installed on your system.
+
+Once the Calculator program is running, you can perform various operations by following the on-screen instructions. Simply enter the numbers and choose the desired operation.
+
+Examples
+Here are some examples of how to use the Calculator program:
+
+Addition:
+
+Enter the first number: 5
+Enter the second number: 3
+Result: 8
+Subtraction:
+
+Enter the first number: 10
+Enter the second number: 4
+Result: 6
+Multiplication:
+
+Enter the first number: 7
+Enter the second number: 6
+Result: 42
+Division:
+
+Enter the first number: 20
+Enter the second number: 5
+Result: 4
+Modulo:
+
+Enter the first number: 15
+Enter the second number: 7
+Result: 1 (remainder)
+Contributing
+Contributions to the Calculator program are welcome. If you find any bugs, have feature requests, or would like to contribute code improvements, please create an issue or submit a pull request on the GitHub repository.
+
+License
+The Calculator program is open source and released under the MIT License. Feel free to modify and distribute it according to the terms of the license.
+
+Contact
+If you have any questions or need assistance, you can reach out to the project maintainer at your@email.com.
+
+Thank you for using the Calculator program! We hope it helps simplify your calculations.
--- main.js
+++ main.js
@@ -1,149 +1,156 @@
-console.log('Hello World!');
+console.log("Hello World!");
+
+document.querySelector("body").innerHTML=`        <div class="container-fluid" id="xxx">
+            <div class="scroll" id="scroll">
+                <div class="containe">
+                    <div class="left">l</div>
+                    <div class="center">
+                        <div class="nav">
+                            <div class="me"><i class="fa fa-commenting" aria-hidden="true"></i></div>
+                            <div class="wifi"><i class="fa fa-wifi" aria-hidden="true"></i></div>
+                            <div class="sig"><i class="fa fa-signal" aria-hidden="true"></i></div>
+                            <div class="si"><i class="fa fa-signal" aria-hidden="true"></i></div>
+                            <div class="b"><i class="fa fa-battery-three-quarters" aria-hidden="true"></i></div>
+                        </div>
+                        <input type="text" id="number" readonly="readonly" />
+                        <div class="sw">
+                            <div class="s"><div class="swi"></div></div>
+                        </div>
+                        <div class="btn-list">
+                            <input type="button" value="1" /> <input type="button" value="2" /> <input type="button" value="3" /> <input type="button" value="+" /> <input type="button" value="4" /> <input type="button" value="5" />
+                            <input type="button" value="6" /> <input type="button" value="-" /> <input type="button" value="7" /> <input type="button" value="8" /> <input type="button" value="9" /> <input type="button" value="*" />
+                            <input type="button" value="0" /> <input type="button" value="." /> <input type="button" value="/" /> <input type="button" value="C" onclick="clearResult()" />
+                        </div>
+                        <div class="re"><input type="button" id="eq" value="p" onclick="pre()" /> <input type="button" id="equal" value="del" onclick="del()" /> <input type="button" id="equal" value="=" onclick="calculateResult()" /></div>
+                    </div>
+                    <div class="center-bottom">center</div>
+                    <div class="right">right</div>
+                    <div class="top">top</div>
+                    <div class="bottom">bottom</div>
+                </div>
+            </div>
+        </div>`;
 
 
 
-var contain=document.querySelector(".containe");
-var input=document.getElementById("number");
-var inpv=input.value;
-var sw=document.querySelector(".swi");
-var s=document.querySelector(".s");
-var experetion=" ";
-sw.addEventListener("click",swit);
-let ct=1;
-function swit(){
-  if(ct===1){
-    ct=0;
-    sw.style.marginLeft="30px";
-    s.style.background="white";
-    contain.style.transform="rotateX(45deg)";
-    
-  }else{
-    ct=1;
-        sw.style.marginLeft="0px";
-             s.style.background="red"; 
-             contain.style.transform="translate(-14%,13%)";
-  }};
- 
- let btnl=document.querySelector(".btn-list");
-let btn=btnl.querySelectorAll("input[type='button']");
-const data=[];
-let arry=Array.from(btn);
-   arry.splice(arry.length-1,1)
-   arry.forEach((e,ind)=>{
-e.addEventListener("click",inputNum)
-})
 
-function inputNum(e){
-  if( input.value==="Erorr"){
-  input.value="";
-  }
-  input.value += e.target.value;
-let audio=new Audio("/audio/Shutter Click sound.mp3");
 
-audio.playbackRate=3;
 
-audio.play();
-  
-  
-  
-  
-  
+
+
+
+var contain = document.querySelector(".containe");
+var input = document.getElementById("number");
+var inpv = input.value;
+var sw = document.querySelector(".swi");
+var s = document.querySelector(".s");
+var experetion = " ";
+sw.addEventListener("click", swit);
+let ct = 1;
+function swit() {
+    if (ct === 1) {
+        ct = 0;
+        sw.style.marginLeft = "30px";
+        s.style.background = "white";
+        contain.style.transform = "rotateX(45deg)";
+    } else {
+        ct = 1;
+        sw.style.marginLeft = "0px";
+        s.style.background = "red";
+        contain.style.transform = "translate(-14%,13%)";
+    }
 }
 
-document.querySelector(".swi").addEventListener("click",()=>{
-  
-  let audio=new Audio("/audio/Nintendo Switch.mp3")
-  audio.playbackRate=5;
-  audio.play();
+let btnl = document.querySelector(".btn-list");
+let btn = btnl.querySelectorAll("input[type='button']");
+const data = [];
+let arry = Array.from(btn);
+arry.splice(arry.length - 1, 1);
+arry.forEach((e, ind) => {
+    e.addEventListener("click", inputNum);
+});
 
-})
+function inputNum(e) {
+    if (input.value === "Erorr") {
+        input.value = "";
+    }
+    input.value += e.target.value;
+    let audio = new Audio("/audio/Shutter Click sound.mp3");
 
+    audio.playbackRate = 3;
 
-
-
-
-
-
-
-
-
-function clearResult(){
-  input.value="";
+    audio.play();
 }
 
+document.querySelector(".swi").addEventListener("click", () => {
+    let audio = new Audio("/audio/Nintendo Switch.mp3");
+    audio.playbackRate = 5;
+    audio.play();
+});
 
-function del(){
-  let v=input.value;
- if(v>0){
-  let up=v.slice(0,-1);
-    input.value=up;
-}};
+function clearResult() {
+    input.value = "";
+}
 
+function del() {
+    let v = input.value;
+    if (v > 0) {
+        let up = v.slice(0, -1);
+        input.value = up;
+    }
+}
 
-function pre(){
-  for(let i=0;i<data.length;i++){
-    input.value=data[i];
-  }
-   data.splice(data.length-1,1);
-  }
-
+function pre() {
+    for (let i = 0; i < data.length; i++) {
+        input.value = data[i];
+    }
+    data.splice(data.length - 1, 1);
+}
 
 //.......................
 
+try {
+    function calculateResult() {
+        //genaret sound effect....
 
+        let audio = new Audio("/audio/Bell Sound.mp3");
+        audio.playbackRate = 5;
+        audio.play();
 
-try{
+        let ip = input.value;
+        let ipl = ip.length;
 
-function calculateResult(){
-  
-    //genaret sound effect....
-  
-  let audio=new Audio("/audio/ Bell Sound.mp3");
-  audio.playbackRate=5;
-  audio.play();
-  
-  
-  
-  
-  
-  let ip=input.value;
-  let ipl=ip.length;
-  
-  let icp=ip.charAt(ipl-1)
-  //console.log(ipl)
-  
-  
-  
-  
-  
-     if (icp === "+" || icp === "-" || icp === "*" || icp === "/") {
-      input.value = "Erorr";
+        let icp = ip.charAt(ipl - 1);
+        //console.log(ipl)
+
+        if (icp === "+" || icp === "-" || icp === "*" || icp === "/") {
+            input.value = "Erorr";
+        }
+
+        let ic = ip.charAt(0);
+
+        if (ic === "+" || ic === "-" || ic === "*" || ic === "/") {
+            input.value = 0;
+        }
+
+        if (ip === "") {
+            input.value = 0;
+        }
+
+        if (input.value === "Erorr") {
+            input.value = 0;
+        }
+
+        if (ip.includes("++") || ip.includes("+/") || ip.includes("+*") || ip.includes("*/") || ip.includes("/*")) {
+            input.value = "Erorr";
+        }
+
+        data.push(input.value);
+        // console.log(data)
+        let res = eval(input.value);
+        let resu = res.toFixed(2);
+        input.value = resu;
     }
-  
-  
-  
-  let ic=ip.charAt(0);
-  
-  if(ic==="+" || ic==="-" ||ic==="*" ||ic==="/"){
-   input.value=0;
-  }
-  
-if(ip===""){
-    input.value=0;}
-    
-   if (input.value === "Erorr") {
-    input.value = 0;}
-  
-  if(ip.includes("++") || ip.includes("+/") || ip.includes("+*") || ip.includes("*/") || ip.includes("/*")){
-    input.value="Erorr"; }
-  
-  data.push(input.value)
- // console.log(data)
- let res=eval(input.value);
-  let resu=res.toFixed(2)
- input.value=resu;}}catch(err){
-  console.log("vff")
-  
-  
-
+} catch (err) {
+    console.log("vff");
 }
